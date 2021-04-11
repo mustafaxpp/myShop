53637a7316a0d200be43484fe959a92b637b7ab0
mustafaxpp <kampawbas@gmail.com>
2021-04-11 05:51:46 +0200

    Fix

--- resources/views/layouts/guestuser.blade.php
+++ resources/views/layouts/guestuser.blade.php
@@ -136,7 +136,6 @@
                                                                                         <button type="button"
                                                                                             class="inline-flex items-center px-2 py-1 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
                                                                                             {{ Auth::user()->name }}
-
                                                                                             <svg class="ml-2 -mr-0.5 h-4 w-4"
                                                                                                 xmlns="http://www.w3.org/2000/svg"
                                                                                                 viewBox="0 0 20 20"
@@ -183,11 +182,11 @@
                                                                                         {{ __('Log Out') }}
                                                                                     </x-jet-dropdown-link>
                                                                                 </form>
+
                                                                             </x-slot>
                                                                         </x-jet-dropdown>
                                                                     </div>
-
-                                                                @elseif (Route::has('register'))
+                                                            @elseif (Route::has('register'))
                                                                     {{-- <a href="{{ route('register') }}"class="nav-item nav-link">Register</a>
                                             <a href="{{ route('login') }}"class="nav-item nav-link">Log in </a> --}}
                                                                     <li class="nav-item dropdown">
