b79ee9f31ef32143d00cb11e1cc3e03aa4cefd42
islam kabbary <islamkabbarygmailcom>
2021-04-21 00:37:29 +0200

    login and register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,37 +8,39 @@
                     </div>
                     <div class="card-body">
                         <h5 class="card-title text-center">Register</h5>
-                        <form class="form-register">
+                        <form class="form-register" method="POST" action="{{ route('register') }}">
+                            @csrf
                             <div class="form-label-group">
-                                <input type="text" id="inputUserame" class="form-control" placeholder="Username"
-                                    required autofocus>
+                                <input type="text" name="name" id="inputUserame" class="form-control"
+                                    placeholder="Username" required autofocus>
                                 <label for="inputUserame">Username</label>
                             </div>
 
                             <div class="form-label-group">
-                                <input type="email" id="inputEmail" class="form-control" placeholder="Email address"
-                                    required>
+                                <input type="email" name="email" id="inputEmail" class="form-control"
+                                    placeholder="Email address" required>
                                 <label for="inputEmail">Email address</label>
                             </div>
 
                             <hr>
 
                             <div class="form-label-group">
-                                <input type="password" id="inputPassword" class="form-control" placeholder="Password"
-                                    required>
+                                <input type="password" name="password" id="inputPassword" class="form-control"
+                                    placeholder="Password" required>
                                 <label for="inputPassword">Password</label>
                             </div>
 
                             <div class="form-label-group">
-                                <input type="password" id="inputConfirmPassword" class="form-control"
-                                    placeholder="Password" required>
+                                <input type="password" name="password_confirmation" id="inputConfirmPassword"
+                                    class="form-control" placeholder="Password" required>
                                 <label for="inputConfirmPassword">Confirm password</label>
                             </div>
-
                             <button class="btn btn-lg btn-primary btn-block text-uppercase"
                                 type="submit">Register</button>
-                            <button class="btn btn-lg btn-success btn-block text-uppercase"
-                                type="submit">Sign In</button>
+
+                            <a href="{{ route('login') }}" class="btn btn-lg btn-success btn-block text-uppercase"
+                                type="submit">Sign
+                                In</a>
                             {{-- <a class="d-block text-center mt-2 text-uppercase" href="#">Sign In</a> --}}
                             <hr class="my-4">
                             <a href="{{ url('auth/google') }}" class="btn btn-lg btn-google btn-block text-uppercase"
